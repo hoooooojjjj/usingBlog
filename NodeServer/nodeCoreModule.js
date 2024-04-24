ed2a21a2a4ce5eb67abad61f0172c73b8222dbe9
sono <rhj080471@gmail.com>
2024-04-24 23:39:11 +0900

    Node.js : 3 fs 모듈(노드 코어 모듈 2)

--- NodeServer/nodeCoreModule.js
+++ NodeServer/nodeCoreModule.js
@@ -1,30 +1,125 @@
-const path = require("node:path");
+const fs = require("node:fs");
+const fsPromise = require("node:fs/promises");
 
-// join()
-// const fullPath = path.join("sono", "blog", "pathModule.js");
-// console.log(fullPath);
+// mkdirSync(path[, options])
+// fs.mkdirSync("madeDir");
+// fs.mkdir("madeDirToCallBack", (err) => {
+//   if (err) throw err;
+// });
+// (async function () {
+//   try {
+//     await fsPromise.mkdir("madeDirToPromise");
+//   } catch (error) {
+//     throw error;
+//   }
+// })();
 
-// sep
-// const fullPath = path.join("sono", "blog", "pathModule.js");
-// const sepPath = fullPath.split(path.sep);
-// console.log(sepPath);
+// fs.writeFileSync("./madeDir/madeFile.txt", "1. 동기 방식으로 파일 생성");
+// fs.writeFile(
+//   "./madeDirToCallBack/madeFileToCallBack.txt",
+//   "2. 콜백함수로 파일 생성",
+//   (err) => {
+//     if (err) throw err;
+//   }
+// );
+// (async function () {
+//   try {
+//     await fsPromise.writeFile(
+//       "./madeDirToPromise/madeFileToPromise.txt",
+//       "3. Promise로 파일 생성"
+//     );
+//   } catch (error) {
+//     throw error;
+//   }
+// })();
 
-// dirname()
-// const fullPath = path.join("sono", "blog", "pathModule.js");
-// const dir = path.dirname(fullPath);
-// console.log(dir);
+// const madeFile = fs.readFileSync("./madeDir/madeFile.txt", {
+//   encoding: "utf8",
+// });
+// console.log(madeFile);
 
-// basename()
-// const fullPath = path.join("sono", "blog", "pathModule.js");
-// const file = path.basename(fullPath, ".js");
-// console.log(file);
+// fs.readFile(
+//   "./madeDirToCallBack/madeFileToCallBack.txt",
+//   {
+//     encoding: "utf8",
+//   },
+//   (err, data) => {
+//     if (err) {
+//       throw err;
+//     } else {
+//       console.log(data);
+//     }
+//   }
+// );
+// (async function () {
+//   try {
+//     const madeFileToPromise = await fsPromise.readFile(
+//       "./madeDirToPromise/madeFileToPromise.txt",
+//       {
+//         encoding: "utf8",
+//       }
+//     );
+//     console.log(madeFileToPromise);
+//   } catch (error) {
+//     throw error;
+//   }
+// })();
 
-// extname()
-// const fullPath = path.join("sono", "blog", "pathModule.js");
-// const extension = path.extname(".index");
-// console.log(extension);
+// const NodeServerDir = fs.readdirSync(
+//   "/Users/ryuhojun/Documents/usingBlog/NodeServer"
+// );
+// console.log(NodeServerDir);
 
-// parse()
-const parsedPath = path.parse(__filename);
-console.log(__filename);
-console.log(parsedPath);
+// fs.readdir("/Users/ryuhojun/Documents/usingBlog/NodeServer", (err, files) => {
+//   if (err) {
+//     throw err;
+//   } else {
+//     console.log(files);
+//   }
+// });
+
+// (async function () {
+//   try {
+//     const NodeServerDir = await fsPromise.readdir(
+//       "/Users/ryuhojun/Documents/usingBlog/NodeServer"
+//     );
+//     console.log(NodeServerDir);
+//   } catch (error) {
+//     throw error;
+//   }
+// })();
+
+// const madeDir = fs.readdirSync(
+//   "/Users/ryuhojun/Documents/usingBlog/NodeServer/madeDir"
+// );
+// console.log(madeDir[0]);
+// fs.unlinkSync(
+//   "/Users/ryuhojun/Documents/usingBlog/NodeServer/madeDir/" + madeDir[0]
+// );
+// fs.rmdirSync("/Users/ryuhojun/Documents/usingBlog/NodeServer/madeDir");
+
+// fs.unlink(
+//   "/Users/ryuhojun/Documents/usingBlog/NodeServer/madeDirToCallBack/madeFileToCallBack.txt",
+//   (err) => {
+//     if (err) throw err;
+//   }
+// );
+// fs.rmdir(
+//   "/Users/ryuhojun/Documents/usingBlog/NodeServer/madeDirToCallBack",
+//   (err) => {
+//     if (err) throw err;
+//   }
+// );
+
+(async function () {
+  try {
+    await fsPromise.unlink(
+      "/Users/ryuhojun/Documents/usingBlog/NodeServer/madeDirToPromise/madeFileToPromise.txt"
+    );
+    await fsPromise.rmdir(
+      "/Users/ryuhojun/Documents/usingBlog/NodeServer/madeDirToPromise"
+    );
+  } catch (error) {
+    throw error;
+  }
+})();
