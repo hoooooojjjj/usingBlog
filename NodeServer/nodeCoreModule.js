a63865851c8894cff964d793a7917a29bc953ffc
sono <rhj080471@gmail.com>
2024-04-25 13:18:23 +0900

    Node.js : 4 http 모듈(노드 코어 모듈 3)

--- NodeServer/nodeCoreModule.js
+++ NodeServer/nodeCoreModule.js
@@ -1,125 +1,20 @@
-const fs = require("node:fs");
-const fsPromise = require("node:fs/promises");
+const http = require("http");
 
-// mkdirSync(path[, options])
-// fs.mkdirSync("madeDir");
-// fs.mkdir("madeDirToCallBack", (err) => {
-//   if (err) throw err;
-// });
-// (async function () {
-//   try {
-//     await fsPromise.mkdir("madeDirToPromise");
-//   } catch (error) {
-//     throw error;
-//   }
-// })();
-
-// fs.writeFileSync("./madeDir/madeFile.txt", "1. 동기 방식으로 파일 생성");
-// fs.writeFile(
-//   "./madeDirToCallBack/madeFileToCallBack.txt",
-//   "2. 콜백함수로 파일 생성",
-//   (err) => {
-//     if (err) throw err;
-//   }
-// );
-// (async function () {
-//   try {
-//     await fsPromise.writeFile(
-//       "./madeDirToPromise/madeFileToPromise.txt",
-//       "3. Promise로 파일 생성"
-//     );
-//   } catch (error) {
-//     throw error;
-//   }
-// })();
-
-// const madeFile = fs.readFileSync("./madeDir/madeFile.txt", {
-//   encoding: "utf8",
-// });
-// console.log(madeFile);
-
-// fs.readFile(
-//   "./madeDirToCallBack/madeFileToCallBack.txt",
-//   {
-//     encoding: "utf8",
-//   },
-//   (err, data) => {
-//     if (err) {
-//       throw err;
-//     } else {
-//       console.log(data);
-//     }
-//   }
-// );
-// (async function () {
-//   try {
-//     const madeFileToPromise = await fsPromise.readFile(
-//       "./madeDirToPromise/madeFileToPromise.txt",
-//       {
-//         encoding: "utf8",
-//       }
-//     );
-//     console.log(madeFileToPromise);
-//   } catch (error) {
-//     throw error;
-//   }
-// })();
-
-// const NodeServerDir = fs.readdirSync(
-//   "/Users/ryuhojun/Documents/usingBlog/NodeServer"
-// );
-// console.log(NodeServerDir);
-
-// fs.readdir("/Users/ryuhojun/Documents/usingBlog/NodeServer", (err, files) => {
-//   if (err) {
-//     throw err;
-//   } else {
-//     console.log(files);
-//   }
-// });
-
-// (async function () {
-//   try {
-//     const NodeServerDir = await fsPromise.readdir(
-//       "/Users/ryuhojun/Documents/usingBlog/NodeServer"
-//     );
-//     console.log(NodeServerDir);
-//   } catch (error) {
-//     throw error;
-//   }
-// })();
-
-// const madeDir = fs.readdirSync(
-//   "/Users/ryuhojun/Documents/usingBlog/NodeServer/madeDir"
-// );
-// console.log(madeDir[0]);
-// fs.unlinkSync(
-//   "/Users/ryuhojun/Documents/usingBlog/NodeServer/madeDir/" + madeDir[0]
-// );
-// fs.rmdirSync("/Users/ryuhojun/Documents/usingBlog/NodeServer/madeDir");
-
-// fs.unlink(
-//   "/Users/ryuhojun/Documents/usingBlog/NodeServer/madeDirToCallBack/madeFileToCallBack.txt",
-//   (err) => {
-//     if (err) throw err;
-//   }
-// );
-// fs.rmdir(
-//   "/Users/ryuhojun/Documents/usingBlog/NodeServer/madeDirToCallBack",
-//   (err) => {
-//     if (err) throw err;
-//   }
-// );
-
-(async function () {
-  try {
-    await fsPromise.unlink(
-      "/Users/ryuhojun/Documents/usingBlog/NodeServer/madeDirToPromise/madeFileToPromise.txt"
-    );
-    await fsPromise.rmdir(
-      "/Users/ryuhojun/Documents/usingBlog/NodeServer/madeDirToPromise"
-    );
-  } catch (error) {
-    throw error;
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-type", "text/plain");
+  const { url, method } = req;
+  if (method === "GET" && url === "/home") {
+    res.write("home page");
+    res.end();
+  } else if (method === "GET" && url === "/about") {
+    res.write("about page");
+    res.end();
+  } else {
+    res.write("404 page Not Found");
+    res.end();
   }
-})();
+});
+
+server.listen(8080, () => {
+  console.log("서버 실행중");
+});
