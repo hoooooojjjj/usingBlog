094da4532c1fb40ce3d3f9b97922e8aa9eeff2f0
sono <rhj080471@gmail.com>
2024-04-29 01:39:37 +0900

    Node.js : 6 Express 미들웨어 1<라우터 미들웨어>(Express 2)

--- NodeServer/app.js
+++ NodeServer/app.js
@@ -2,38 +2,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send("User Page");
-});
-
-app.get("/user/:userid", (req, res) => {
-  const { userid } = req.params; // userid 라우트 파라미터 받아오기
-  res.send(`View UserID : ${userid}`);
-});
-
-app.get("/user/:userid", (req, res) => {
-  const { q } = req.query;
-  const { userid } = req.params;
-  if (q) {
-    res.send(`View ${userid}'s Text : ${q}`);
-  } else {
-    res.send(`View UserID : ${userid}`);
-  }
-});
-
-app.post("/user", (req, res) => {
-  res.send("Create User info");
-});
-
-app.put("/user/:userid", (req, res) => {
-  const { userid } = req.params;
-  res.send(`Edit ${userid}'s info`);
-});
-
-app.delete("/user/:userid", (req, res) => {
-  const { userid } = req.params;
-  res.send(`Delete ${userid}'s info`);
-});
+app.use("/user", require("./routes/user"));
 
 app.listen(8080, () => {
   console.log("서버 실행 중");
---
+++ NodeServer/routes/user.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const router = express.Router();
+
+router
+  .route("/")
+  .get((req, res) => {
+    res.send("User Page");
+  })
+  .post((req, res) => {
+    res.send("Create User info");
+  });
+
+router
+  .route("/:userid")
+  .get((req, res) => {
+    const { q } = req.query;
+    const { userid } = req.params;
+    if (q) {
+      res.send(`View ${userid}'s Text : ${q}`);
+    } else {
+      res.send(`View UserID : ${userid}`);
+    }
+  })
+  .put((req, res) => {
+    const { userid } = req.params;
+    res.send(`Edit ${userid}'s info`);
+  })
+  .delete((req, res) => {
+    const { userid } = req.params;
+    res.send(`Delete ${userid}'s info`);
+  });
+
+module.exports = router;
